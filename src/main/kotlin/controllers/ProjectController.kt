f5aae25bf17c6a4dedf12f656e60de88c4804c8e
gasick <gasick@mail.ru>
2020-10-12 22:48:59 +0300

    delete project function

--- src/main/kotlin/controllers/ProjectController.kt
+++ src/main/kotlin/controllers/ProjectController.kt
@@ -1,9 +1,13 @@
 package dev.alpas.fireplace.controllers
 
 import dev.alpas.fireplace.entities.Projects
+import dev.alpas.fireplace.entities.Projects.idi
 import dev.alpas.ozone.create
 import dev.alpas.http.HttpCall
+import dev.alpas.orAbort
 import dev.alpas.routing.Controller
+import me.liuwj.ktorm.dsl.delete
+import me.liuwj.ktorm.dsl.eq
 import me.liuwj.ktorm.entity.findAll
 
 class ProjectController : Controller() {
@@ -26,5 +30,17 @@ class ProjectController : Controller() {
 
         flash("success", "Successfully added project '${project.title}")
         call.redirect().toRouteNamed("projects.list")
+    }
+
+    fun delete(call: HttpCall){
+        //get the project id
+        //delete the project
+        //flash message tot he user
+        //redirect back to where we came from
+
+        val idi = call.longParam("idi").orAbort()
+        Projects.delete { it.idi eq idi }
+        flash("success", "Successfully deleted a project ")
+        call.redirect().back()
     }
 }
