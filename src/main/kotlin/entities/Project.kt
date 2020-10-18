dfb075de7d4ec45334b4e3ddb801b9fb98f7106d
gasick <gasick@mail.ru>
2020-10-18 13:47:34 +0300

    Displaying Members of a Project

--- src/main/kotlin/entities/Project.kt
+++ src/main/kotlin/entities/Project.kt
@@ -15,6 +15,7 @@ interface Project : OzoneEntity<Project> {
     val owner: User
     val tasks get() = hasMany(Tasks)
     val activities get() = hasMany(Activities)
+    val members get() = hasMany(ProjectMemberships).map{it.member}
     var createdAt: Instant?
     var updatedAt: Instant?
 
